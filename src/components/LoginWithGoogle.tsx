5bc5b0611638331837a5bfacad4996a0a63f920d
Yagth <yabuman46@gmail.com>
2023-08-10 21:03:58 +0300

    Editing google to conform to my backend

--- src/components/LoginWithGoogle.tsx
+++ src/components/LoginWithGoogle.tsx
@@ -1,68 +1,69 @@
-import {GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline} from "react-google-login";
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
 import { redirect } from "react-router-dom";
-const clientId = process.env.GOOGLE_CLIENT_ID ?? ""
-
+const clientId = process.env.GOOGLE_CLIENT_ID ?? "";
 
 const LoginWithGoogle = () => {
-    const responseGoogle = async (response:GoogleLoginResponse) => {
-        try {
-          // Send the response.idToken to your backend for verification
-          const idToken = response?.tokenId; // tokenId is specific to react-google-login
-          console.log(idToken);
-          
-      
-          if (!idToken) {
-            throw new Error("No idToken received");
-          }
-      
-          // Send the idToken to your backend for verification
-          const backendResponse = await fetch("/api/oauth/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ idToken }),
-          });
-      
-          if (!backendResponse.ok) {
-            throw new Error("Authentication failed");
-          }
-      
-          // Handle successful login
-          const responseData = await backendResponse.json();
-          const { user, token } = responseData;
-      
-          // Perform actions based on the user data or token
-          console.log("User:", user);
-          console.log("Token:", token);
-        } catch (error) {
-          // Handle error
-          console.error("Error:", error);
-        }
-      };
-      
+  const responseGoogle = async (response: GoogleLoginResponse) => {
+    try {
+      // Send the response.idToken to your backend for verification
+      const idToken = response?.tokenId; // tokenId is specific to react-google-login
+      console.log(idToken);
 
-    const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-      responseGoogle(res as GoogleLoginResponse)
-      // return redirect("/home")
-    }
+      if (!idToken) {
+        throw new Error("No idToken received");
+      }
 
-    const onFailure = (res: GoogleLoginResponse) => {
-      alert("Login with google faild");
-      return redirect("/login")
+      // Send the idToken to your backend for verification
+      const backendResponse = await fetch("/oauth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: idToken }),
+      });
+
+      if (!backendResponse.ok) {
+        throw new Error("Authentication failed");
+      }
+
+      // Handle successful login
+      const responseData = await backendResponse.json();
+      const { user, token } = responseData;
+
+      // Perform actions based on the user data or token
+      console.log("User:", user);
+      console.log("Token:", token);
+    } catch (error) {
+      // Handle error
+      console.error("Error:", error);
     }
-  
-    return (
-      <div className="mt-3">
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Login with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={"/"}
-        />
-      </div>
-    );
-}
+  };
+
+  const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    responseGoogle(res as GoogleLoginResponse);
+    // return redirect("/home")
+  };
+
+  const onFailure = (res: GoogleLoginResponse) => {
+    alert("Login with google faild");
+    return redirect("/login");
+  };
+
+  return (
+    <div className="mt-3">
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"/"}
+      />
+    </div>
+  );
+};
 
 export default LoginWithGoogle;
